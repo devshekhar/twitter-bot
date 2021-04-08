200c24ccc8de4fabc35e6b08d4497fc1775b9ff7
devshekhar <devshekhar1993@gmail.com>
2021-04-09 01:14:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyparser = require('body-parser');
+var cors = require('cors');
+require('dotenv').config();
+var mongoose = require('mongoose');
+var twitts = require('./api-routes/twitts');
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(process.env.MONGODBURL,(err,result) =>{
+    if(err) {
+        console.log(err,333)
+    }else{
+        console.log('Database connected successfully');
+    }
+
+})
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use('/twitts',twitts);
+app.get("/test", function (req, res) {
+    res.send("test")
+    })
+
+app.listen(3000,() =>{
+    console.log(`bot is listining on ${process.env.PORT}`)
+})
